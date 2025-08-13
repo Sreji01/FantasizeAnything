4bbe899bb86163830a7b8adfc7631696e1f4bc0e
Sreji01 <marko.srejic01@gmail.com>
2025-08-13 21:10:25 +0200

    Initail commit

---
+++ Backend/src/main/java/com/fantasize_anything/app/FantasizeAnythingApplication.java
@@ -0,0 +1,13 @@
+package com.fantasize_anything.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FantasizeAnythingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FantasizeAnythingApplication.class, args);
+	}
+
+}
